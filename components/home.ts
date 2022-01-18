bbffd84df0e2701cc13d15e505b3778e0eb2caec
Fabiano Pereira <fabianop971@gmail.com>
2022-01-17 21:45:33 -0300

    Styles and Structure (About): Almost finish

--- components/home.ts
+++ components/home.ts
@@ -187,3 +187,68 @@ export const Circle = styled.a<{
     transform: scale(1.1, 1.1);
   }
 `
+
+//==> About
+export const Section = styled.section`
+  width: 100%;
+  min-height: 100vh;
+  background-color: var(--white);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
+export const Img = styled.img`
+  max-width: 70%;
+  max-height: 100vh;
+`
+export const BannerVertical = styled.article<{
+  background: string
+}>`
+  width: 30%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  padding: 1rem 3rem;
+  background-color: var(--blue-normal);
+  position: relative;
+
+  & p:first-of-type {
+    font-size: var(--size-md);
+    z-index: 2;
+  }
+
+  & p:last-of-type {
+    margin-top: 1rem;
+    text-align: justify;
+    color: var(--gray);
+  }
+
+  & a {
+    font-size: var(--size-md);
+    margin: 0 auto;
+    margin-top: 2rem;
+  }
+
+  &::before {
+    content: '';
+    width: 100%;
+    height: 7rem;
+    background-color: var(--orange-light);
+    position: absolute;
+    top: 0;
+    left: 0;
+    border-radius: 0 0 90% 90%;
+    z-index: 1;
+  }
+`
+export const H5 = styled.h5`
+  text-transform: uppercase;
+  color: var(--white);
+  font-family: var(--font-primary), sans-serif;
+  font-size: var(--size-l);
+  font-weight: 500;
+  letter-spacing: 0.3rem;
+  z-index: 2;
+`
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,65 +4,97 @@ import * as Fa from 'react-icons/fa'
 import * as Fi from 'react-icons/fi'
 import Link from 'next/link'
 import bg from '../public/assets/vetor_home.svg'
+import bannerVertical from '../public/assets/vetor_message_about.svg'
+import groupTi from '../public/assets/vetor_about.svg'
 
 const Home = () => {
   return (
-    <C.Container>
-      <C.Header>
-        <C.ContainerLogo>
-          <Fa.FaRegCalendarAlt />
-          <C.Strong>Validay</C.Strong>
-        </C.ContainerLogo>
+    <>
+      <C.Container>
+        <C.Header>
+          <C.ContainerLogo>
+            <Fa.FaRegCalendarAlt />
+            <C.Strong>Validay</C.Strong>
+          </C.ContainerLogo>
 
-        <C.Navbar>
-          <Link href="home">
-            <C.Anchor aria-label="Anchor para inicio">Inicio</C.Anchor>
-          </Link>
+          <C.Navbar>
+            <Link href="home">
+              <C.Anchor aria-label="Anchor para inicio">Inicio</C.Anchor>
+            </Link>
+            <Link href="about">
+              <C.Anchor aria-label="Anchor para sobre">Sobre</C.Anchor>
+            </Link>
+            <Link href="services">
+              <C.Anchor aria-label="Anchor para serviços">Serviços</C.Anchor>
+            </Link>
+            <Link href="plans">
+              <C.Anchor aria-label="Anchor para planos">Planos</C.Anchor>
+            </Link>
+          </C.Navbar>
+          <C.NavbarSecundary>
+            <Link href="/login">
+              <C.Anchor aria-label="Anchor para acessar painel">
+                Acessar
+              </C.Anchor>
+            </Link>
+            <Link href="/register">
+              <C.AnchorBlue
+                marginLeft={1}
+                aria-label="Anchor para se registrar"
+              >
+                Registrar
+              </C.AnchorBlue>
+            </Link>
+          </C.NavbarSecundary>
+        </C.Header>
+        <C.Main background={bg.src}>
+          <C.H1>Validay</C.H1>
+          <C.H3>Uma solução moderna.</C.H3>
+          <C.P marginTop={1}>
+            Forneça uma melhor experiência aos seus clientes fazendo um melhor
+            controle da validade de seus produtos.
+          </C.P>
           <Link href="about">
-            <C.Anchor aria-label="Anchor para sobre">Sobre</C.Anchor>
-          </Link>
-          <Link href="services">
-            <C.Anchor aria-label="Anchor para serviços">Serviços</C.Anchor>
-          </Link>
-          <Link href="plans">
-            <C.Anchor aria-label="Anchor para planos">Planos</C.Anchor>
-          </Link>
-        </C.Navbar>
-        <C.NavbarSecundary>
-          <Link href="/login">
-            <C.Anchor aria-label="Anchor para acessar painel">Acessar</C.Anchor>
-          </Link>
-          <Link href="/register">
-            <C.AnchorBlue marginLeft={1} aria-label="Anchor para se registrar">
-              Registrar
+            <C.AnchorBlue
+              marginTop={5}
+              height={3}
+              width={20}
+              aria-label="link para conhecer o sistema validay"
+            >
+              Quero conhecer
             </C.AnchorBlue>
           </Link>
-        </C.NavbarSecundary>
-      </C.Header>
-      <C.Main background={bg.src}>
-        <C.H1>Validay</C.H1>
-        <C.H3>Uma solução moderna.</C.H3>
-        <C.P marginTop={1}>
-          Forneça uma melhor experiência aos seus clientes fazendo um melhor
-          controle da validade de seus produtos.
-        </C.P>
-        <Link href="about">
-          <C.AnchorBlue
-            marginTop={5}
-            height={3}
-            width={20}
-            aria-label="link para conhecer o sistema validay"
-          >
-            Quero conhecer
-          </C.AnchorBlue>
-        </Link>
-        <Link href="home">
-          <C.Circle>
-            <Fi.FiCornerRightUp />
-          </C.Circle>
-        </Link>
-      </C.Main>
-    </C.Container>
+          <Link href="home">
+            <C.Circle>
+              <Fi.FiCornerRightUp />
+            </C.Circle>
+          </Link>
+        </C.Main>
+      </C.Container>
+      <C.Section>
+        <C.Img
+          src={groupTi.src}
+          alt="Imagem de jovens de tecnologia trabalhando."
+        />
+        <C.BannerVertical background={bannerVertical.src}>
+          <C.H5>Sobre</C.H5>
+          <C.P color="var(--blue-dark)">Conheça nosso sistema.</C.P>
+          <C.P>
+            Forneça uma melhor experiência aos seus clientes fazendo um melhor
+            controle da validade de seus produtos.Forneça uma melhor experiência
+            aos seus clientes fazendo um melhor controle da validade de seus
+            produtos. Forneça uma melhor experiência aos seus clientes fazendo
+            um melhor controle da validade de seus produtos.Forneça uma melhor
+            experiência aos seus clientes fazendo um melhor controle da validade
+            de seus produtos. Forneça uma melhor experiência aos seus clientes
+            fazendo um melhor controle da validade de seus produtos.
+          </C.P>
+          <Link href="plans">
+            <C.AnchorBlue>Conhecer planos</C.AnchorBlue>
+          </Link>
+        </C.BannerVertical>
+      </C.Section>
+    </>
   )
 }
 
