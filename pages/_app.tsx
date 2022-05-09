0a47a7152856b44a06ec20068eb3523d8f2ba3bd
Fabiano Pereira <fabianop971@gmail.com>
2022-05-09 00:31:48 -0300

    Error: Resolve

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,14 @@
 import type { AppProps } from 'next/app'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
-import AuthContextProvider from '../context/authContext'
-import ProductsProvider from '../context/productContext'
 import '../styles/globals.css'
 
 function GVP({ Component, pageProps }: AppProps) {
   return (
-    <AuthContextProvider context={pageProps}>
-      <ProductsProvider>
-        <ToastContainer />
-        <Component {...pageProps} />
-      </ProductsProvider>
-    </AuthContextProvider>
+    <>
+      <ToastContainer />
+      <Component {...pageProps} />
+    </>
   )
 }
 
