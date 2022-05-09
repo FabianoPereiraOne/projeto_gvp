5b7896a3193a6b53c05f4a5cce15565cffbaf357
Fabiano Pereira <fabianop971@gmail.com>
2022-05-09 00:11:57 -0300

    Altered

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,16 @@
+import Head from 'next/head'
 import { About } from '../components/Home/About'
 import { Footer } from '../components/Home/Footer'
-import { Services } from '../components/Home/Services'
-import { Plans } from '../components/Home/Plans'
 import { Main } from '../components/Home/Main'
+import { Plans } from '../components/Home/Plans'
+import { Services } from '../components/Home/Services'
 import '../styles/home'
-import Head from 'next/head'
 
 const Home = () => {
   return (
     <>
       <Head>
-        <title>GVP | Gerenciador de validade de produtos</title>
+        <title>GVP | Gerenciador de validade de produtoS</title>
       </Head>
       <Main />
       <About />
