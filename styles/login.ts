679a649a7131866f4f9bb3d857d6ad01187bfdf9
Fabiano Pereira <fabianop971@gmail.com>
2022-05-30 12:10:51 -0300

    Styles (Login/register): Add styled-components

--- styles/login.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components'
-
-export const Form = styled.form``
-export const FormGroup = styled.div``
-export const Label = styled.label``
-export const Input = styled.input``
-export const ButtonSubmit = styled.button``
